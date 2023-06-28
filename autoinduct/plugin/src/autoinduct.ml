16077cc67477d9dc35f8c2e4b0681ac522149e3c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-06-28 11:04:34 +0200

    Separate finding induction arguments and doing induction
    Also properly recurse on subterms

--- autoinduct/plugin/src/autoinduct.ml
+++ autoinduct/plugin/src/autoinduct.ml
@@ -36,16 +36,9 @@ let branch_state p f g a =
     g a
   end
 
-(* Like List.fold_left, but threading state *)
-let fold_left_state = State.List.fold_left
-
 (* fold_left2 with state *)
 let fold_left2_state f acc l1 l2 =
   State.List.fold_left2 (fun _ -> failwith "not same length") f acc l1 l2
-
-(* Like fold_left_state, but over arrays *)
-let fold_left_state_array f b args =
-  fold_left_state f b (Array.to_list args)
 
 (* Like fold_left2_state, but over arrays *)
 let fold_left2_state_array f c args1 args2 sigma =
@@ -100,40 +93,47 @@ let rec recursive_argument env f_body sigma =
 
 (*
  * Inner implementation of autoinduct tactic
- * The current version supports only nested applications, and doesn't support every kind of subterm yet
+ * The current version does not go under binders.
  * It's Part 1 out of 3, so requires explicit arguments
  * It also does not have the most useful error messages
  * It also requires exact equality (rather than convertibility) for the function and all of its arguments
  * It also may not stop itself if the chosen argument is not inductive (have not tested yet; it may, actually)
  *)
-let rec do_autoinduct env concl f f_args sigma =
-  match kind sigma concl with
-  | Constr.App (g, g_args) ->
-     let sigma, f_eq = eequal f g sigma in
-     if f_eq then
-       if Array.length f_args = Array.length g_args then
-         let sigma, args_eq = forall2_state_array eequal f_args g_args sigma in
-         if args_eq then 
-           let f_body = lookup_definition env f sigma in
-           let arg_no = recursive_argument env f_body sigma in
-           let arg = Array.get g_args arg_no in
-           let dest_arg = (Some true), Tactics.ElimOnConstr (fun env sigma -> sigma, (arg, Tactypes.NoBindings)) in
-           Tactics.induction_destruct true false ([(dest_arg, (None, None), None)], None)
-         else
-           Tacticals.tclFAIL (Pp.str "Wrong number of arguments")
-       else
-         Tacticals.tclFAIL (Pp.str "Could not find an occurrence of the supplied function")
-     else
-       let _, t =
-         fold_left_state_array
-           (fun t a sigma ->
-             sigma, Tacticals.tclOR t (do_autoinduct env a f f_args sigma))
-           (Tacticals.tclFAIL (Pp.str "No arguments were supplied"))
-           g_args
-           sigma
-       in t
-  | _ ->
-     Tacticals.tclFAIL (Pp.str "Could not find anything to induct over")
+let find_autoinduct env f f_args concl sigma =
+  let rec aux under_binders (sigma,found as acc) concl =
+    if under_binders then acc
+    else
+      let recurse (sigma, _ as acc) =
+        EConstr.fold_with_binders sigma (fun _ -> true) aux under_binders acc concl
+      in
+      match kind sigma concl with
+      | App (g, g_args) ->
+        let sigma, f_eq = eequal f g sigma in
+        if f_eq then
+          let sigma, args_eq = forall2_state_array eequal f_args g_args sigma in
+          if args_eq then
+            let f_body = lookup_definition env f sigma in
+            let arg_no = recursive_argument env f_body sigma in
+            let arg = g_args.(arg_no) in
+            recurse (sigma, arg :: found)
+          else recurse acc
+        else recurse acc
+      | _ -> recurse acc
+  in
+  aux false (sigma,[]) concl
+
+let one_autoinduct arg =
+  let dest_arg = Some true, Tactics.ElimOnConstr (fun _env sigma -> sigma, (arg, Tactypes.NoBindings)) in
+  Tactics.induction_destruct true false ([dest_arg, (None, None), None], None)
+
+let do_autoinduct env concl f f_args sigma =
+  let sigma, induct_args = find_autoinduct env f f_args concl sigma in
+  if CList.is_empty induct_args
+  then Tacticals.tclFAIL Pp.(str "Could not find anything to induct over.")
+  else
+    tclBIND (Proofview.Unsafe.tclEVARS sigma) begin fun () ->
+      Tacticals.tclFIRST (List.map one_autoinduct induct_args)
+    end
 
 (*
  * Implementation of autoinduct tactic, top level
